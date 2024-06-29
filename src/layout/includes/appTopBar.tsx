01be0a7e92d99a30a15f6e0d2d915be1304b8e28
atanusaproy <atanusaproy@gmail.com>
2024-06-29 13:55:11 +0530

    Searchbar updated

--- src/layout/includes/appTopBar.tsx
+++ src/layout/includes/appTopBar.tsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Navbar, Nav, Container, Offcanvas, Button, Form, FormControl, NavDropdown, Spinner } from 'react-bootstrap';
-import { Link, useLocation, useSearchParams } from 'react-router-dom';
-import { IProducts } from '../../Interface/Product.interface';
+import { Navbar, Nav, Container, Button, Form, FormControl, NavDropdown } from 'react-bootstrap';
+import { Link, useLocation, useNavigate, useSearchParams } from 'react-router-dom';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import ShoppingCart from '@mui/icons-material/ShoppingCart';
 import ProductService from '../../Service/ProductService';
+import './appTopBar.css';
 
 interface IProps {
   categoriesMenu?: string[] | [];
   setSearchParams?: any;
 }
-
 
 const pages = [
   {
@@ -26,17 +27,16 @@ const pages = [
 ];
 
 const AppTopBar: React.FC<IProps> = () => {
-
   const location = useLocation();
   const pathname = location.pathname;
   const [showForm, setShowForm] = useState(true);
-
-  const [showOffcanvas, setShowOffcanvas] = useState(false);
   const [categories, setCategories] = useState<string[]>([]);
-
+  const [searchParams, setSearchParams] = useSearchParams();
+  const searchQuery = searchParams.get('search') || '';
+  const categoryQuery = searchParams.get('category') || 'all';
+  const navigate = useNavigate(); // Correctly use the useNavigate hook inside the component
 
   useEffect(() => {
-
     const fetchData = async () => {
       try {
         const categories = await new ProductService().getProductCat();
@@ -47,32 +47,35 @@ const AppTopBar: React.FC<IProps> = () => {
     };
     fetchData();
 
-    if (pathname !== '/' || '') {
-      setShowForm(false);
-    }else{
-      setShowForm(true);
+    // if (pathname !== '/' && pathname !== '') {
+    //   setShowForm(false);
+    // } else {
+    //   setShowForm(true);
+    // }
+  }, [pathname]); // Remove unnecessary dependencies and ensure pathname is correctly checked
+
+  const navigateToHome = ((category: string, searchQuery: string) => {
+    if (pathname !== '/' && pathname !== '') {
+      navigate({
+        pathname: '/',
+        search: `?search=${searchQuery}&category=${category.toLowerCase()}`
+      });
     }
-
-  }, [showForm, categories])
-
-  const [searchParams, setSearchParams] = useSearchParams();
-  const searchQuery = searchParams.get('search') || '';
-  const categoryQuery = searchParams.get('category') || 'all';
-
-  const handleToggleOffcanvas = () => setShowOffcanvas(!showOffcanvas);
-  const handleCloseOffcanvas = () => setShowOffcanvas(false);
+  });
 
   const handleCategoryChange = (category: string) => {
     setSearchParams({ search: searchQuery, category: category.toLowerCase() });
+    navigateToHome(category, searchQuery);
   };
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const query = event.target.value.toLowerCase();
     setSearchParams({ search: query, category: categoryQuery });
+    navigateToHome(categoryQuery, query);
   };
 
-  return (  
-    <> 
+  return (
+    <>
       <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
         <Container>
           <Link to={'/'}>
@@ -83,7 +86,6 @@ const AppTopBar: React.FC<IProps> = () => {
               className="d-inline-block align-top"
               alt="OpenCart Logo"
             />
-
           </Link>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
@@ -96,27 +98,33 @@ const AppTopBar: React.FC<IProps> = () => {
                 ))}
               </NavDropdown>
 
-              {
-                pages && pages.map((res, i) => (
-                  <Nav.Link href={res.link} key={i}>
-                    {res.menuName}
-                  </Nav.Link>
-                ))
-              }
+              {pages && pages.map((res, i) => (
+                <Nav.Link as={Link} to={res.link} key={i}>
+                  {res.menuName}
+                </Nav.Link>
+              ))}
             </Nav>
             {showForm && (
-            <Form className="d-flex">
-              <FormControl
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-                onChange={handleSearchChange}
-                value={searchQuery}
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
+              <Form className="d-flex">
+                <FormControl
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                  onChange={handleSearchChange}
+                  value={searchQuery}
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
             )}
+            <Nav className="ms-auto">
+              <Nav.Link as={Link} to="#">
+                <ShoppingCart className="nav-icon" fontSize="large" />
+              </Nav.Link>
+              <Nav.Link as={Link} to="#">
+                <AccountCircle className="nav-icon" fontSize="large" />
+              </Nav.Link>
+            </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
